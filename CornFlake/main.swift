0f5d0ad65b21c22e65bead76e86236cd96960dd7
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-02-20 17:42:51 -0800

    Initial Commit

---
+++ CornFlake/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CornFlake
+//
+//  Created by Joshua Weinstein on 2/20/16.
+//  Copyright © 2016 Joshua Weinstein. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
